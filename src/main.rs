aaf4b2182f7faf738c673d6f2f52432bb4adfefa
Edward Ruzek <edadiddles@hotmail.com>
2025-03-20 08:27:49 -0400

    adding test harness that prints results to qemu using vga buffer.

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,10 @@
 #![no_std]
 #![no_main]
 
+#![feature(custom_test_frameworks)]
+#![test_runner(crate::test_runner)]
+#![reexport_test_harness_main = "test_main"]
+
 mod vga_buffer;
 
 use core::panic::PanicInfo;
@@ -13,6 +17,26 @@ fn panic(info: &PanicInfo) -> ! {
 #[unsafe(no_mangle)]
 pub extern "C" fn _start() -> ! {
     println!("Hello\nRusty\nPi");
-    panic!("I'm panic");
-    //loop{}
+
+    #[cfg(test)]
+    test_main();
+
+    loop{}
+}
+
+
+// QUES: Move to a separate file?
+#[cfg(test)]
+pub fn test_runner(tests: &[&dyn Fn()]) {
+    println!("Running {} tests", tests.len());
+    for test in tests {
+        test();
+    }
+}
+
+#[test_case]
+fn trivial_assertion() {
+    print!("trivial assertion...");
+    assert_eq!(1,1);
+    println!("[ok]");
 }
